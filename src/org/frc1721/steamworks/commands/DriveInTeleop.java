4678b8870138f973532a54cbf609e7ae98dfa803
Thepiggy <Zacharysamenfeld@comcast.net>
2017-01-21 16:25:47 -0500

    Basic arcade drive

---
+++ src/org/frc1721/steamworks/commands/DriveInTeleop.java
@@ -0,0 +1,34 @@
+package org.frc1721.steamworks.commands;
+
+import org.frc1721.steamworks.Robot;
+
+public class DriveInTeleop {
+
+
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.driveTrain.jInput(Robot.oi.jstick);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.driveTrain.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+	
+}
--- src/org/frc1721/steamworks/subsystems/DriveTrain.java
+++ src/org/frc1721/steamworks/subsystems/DriveTrain.java
@@ -1,11 +1,17 @@
 package org.frc1721.steamworks.subsystems;
 
+import org.frc1721.steamworks.CustomRobotDrive;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class DriveTrain extends Subsystem {
+	
+	
+	protected static CustomRobotDrive m_robotDrive;
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
@@ -13,6 +19,24 @@ public class DriveTrain extends Subsystem {
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    	
+		setDefaultCommand(new DriveInTeleop());
     }
+    
+    
+	public DriveTrain(CustomRobotDrive robotDrive) {
+		super("DriveTrain");
+		m_robotDrive = robotDrive;
+	}
+	
+	public void jInput(Joystick stick) {
+		m_robotDrive.arcadeDrive(stick);
+	}
+	
+	public void stop() {
+		m_robotDrive.drive(0, 0);
+	}
+	
+    
 }
 

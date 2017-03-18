2586caf1ff7a067894f5557a95cbb422f97d079d
James Forsythe <forsytjr@gmail.com>
2017-03-18 10:12:17 -0400

    switched to using old method for gear auto

--- src/org/frc1721/steamworks/commands/AutoDepositGear.java
+++ src/org/frc1721/steamworks/commands/AutoDepositGear.java
@@ -10,6 +10,27 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutoDepositGear extends CommandGroup {
     public  AutoDepositGear(double dir) {
 
+      boolean useOldMethod = true;
+      if (useOldMethod) {
+        addSequential(new SetCoordinates(RobotMap.centerStartX, RobotMap.centerStartY));
+        addSequential(new SetYawOffset(180.0));
+        addSequential(new EnableDrivePIDCommand());
+        double targetX = RobotMap.cGearDepositX - 5.0;
+        // Drive to a point in line with the gear deposit
+        addSequential(new DriveToCoordinates(targetX, 0.0, -2.0));
+        targetX = RobotMap.cGearDepositX - 1.0;
+        // Drive slowly final portion, increase tolerance to give it time on target
+        addSequential(new DriveToCoordinates(targetX, 0.0, -1.0, 0.1, 20));
+        addSequential(new DrivePause(1.50));
+        //addSequential(new DistanceDriveStraight(1.0, 0.5, 0.2));
+        //addSequential(new DriveToCoordinates(targetX, 0.0, -1.0, 0.1, 20));
+        //addSequential(new DrivePause(1.5));
+        addSequential(new DistanceDriveStraight(1.0, 0.5));
+        addSequential(new DistanceDriveStraight(3.0, 4.0));
+        double targetY = dir*RobotMap.quarterFieldWidth;
+        addSequential(new DriveToCoordinates(5.0, targetY, 5.0, 2.0, 2));
+        addSequential(new DriveToCoordinates(14.0,targetY, 5.0, 2.0, 2));  
+      } else {
     	// Set the position
     	addSequential(new SetCoordinates(RobotMap.centerStartX, RobotMap.centerStartY));
     	addSequential(new SetYawOffset(180.0));
@@ -23,28 +44,11 @@ public class AutoDepositGear extends CommandGroup {
     	double targetY = dir*RobotMap.quarterFieldWidth;
     	addSequential(new DriveToCoordinates(5.0, targetY, 5.0, 2.0, 2));
     	addSequential(new DriveToCoordinates(14.0,targetY, 5.0, 2.0, 2));
-
+      }
 /*    // Old Method below, if the above code does not work,
  *    // try commenting it out and using what is below
         // Set the position
-        addSequential(new SetCoordinates(RobotMap.centerStartX, RobotMap.centerStartY));
-        addSequential(new SetYawOffset(180.0));
-        addSequential(new EnableDrivePIDCommand());
-        double targetX = RobotMap.gearDepositX - 5.0;
-        // Drive to a point in line with the gear deposit
-        addSequential(new DriveToCoordinates(targetX, 0.0, -2.0));
-        targetX = RobotMap.gearDepositX - 1.0;
-        // Drive slowly final portion, increase tolerance to give it time on target
-        addSequential(new DriveToCoordinates(targetX, 0.0, -1.0, 0.1, 20));
-        addSequential(new DrivePause(1.50));
-        //addSequential(new DistanceDriveStraight(1.0, 0.5, 0.2));
-        //addSequential(new DriveToCoordinates(targetX, 0.0, -1.0, 0.1, 20));
-        //addSequential(new DrivePause(1.5));
-        addSequential(new DistanceDriveStraight(1.0, 0.5));
-        addSequential(new DistanceDriveStraight(3.0, 4.0));
-        double targetY = dir*RobotMap.quarterFieldWidth;
-        addSequential(new DriveToCoordinates(5.0, targetY, 5.0, 2.0, 2));
-        addSequential(new DriveToCoordinates(14.0,targetY, 5.0, 2.0, 2));
+
 
  */
     

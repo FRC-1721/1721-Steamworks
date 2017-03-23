365a7bd90592c7a2ba446b0e07f1f55ba3c69ec9
James Forsythe <forsytjr@gmail.com>
2017-03-23 17:24:18 -0400

    simplify teleop deposit gear

--- src/org/frc1721/steamworks/commands/TeleopDepositGear.java
+++ src/org/frc1721/steamworks/commands/TeleopDepositGear.java
@@ -11,7 +11,6 @@ public class TeleopDepositGear extends CommandGroup {
     public  TeleopDepositGear() {
     	
     	addSequential(new DriveToGearTarget(5.0, -2.0, 1.0));
-    	addSequential(new DriveToGearTarget(3.0, -2.0, 1.0));
     	addSequential(new DriveToGearTarget(1.0, -1.0, 0.1));
     	addSequential(new DrivePause(1.50));
     	addSequential(new DistanceDriveStraight(1.0, 0.5));
